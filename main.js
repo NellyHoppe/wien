2a760c4ad0cebd5845e878e459df56e14a37cd6a
Nelly Hoppe <nelly.hoppe@student.uibk.ac.at>
2022-05-05 16:36:16 +0200

    Fußgängerzonen implementiert

--- main.js
+++ main.js
@@ -89,3 +89,16 @@ async function loadLines(url) {
     L.geoJson(geojson).addTo(overlay);
 }
 loadLines("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKLINIEVSLOGD&srsName=EPSG:4326&outputFormat=json")
+
+// Fußgängerzonen
+async function loadZones(url) {
+    let response = await fetch(url);
+    let geojson = await response.json();
+    // console.log(geojson);
+
+    let overlay = L.featureGroup();
+    layerControl.addOverlay(overlay, "Fußgängerzonen");
+
+    L.geoJson(geojson).addTo(overlay)
+}
+loadZones("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:FUSSGEHERZONEOGD&srsName=EPSG:4326&outputFormat=json")
