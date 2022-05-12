c4ea3c0126d150579002a3ad918ecb2e954f1e47
Nelly Hoppe <nelly.hoppe@student.uibk.ac.at>
2022-05-12 18:21:39 +0200

    view setzen und popup öffnen bei Klich auf anzeigen

--- main.js
+++ main.js
@@ -242,10 +242,14 @@ async function loadHotels(url) {
     form.suchen.onclick = function() {
         console.log(form.hotel.value);
         hotelsLayer.eachLayer(function (marker) {
-            console.log(marker)
-            console.log(marker.getLatLng())
-            console.log(marker.getPopup())
-            console.log(marker.feature.properties.BETRIEB)
+
+            if (form.hotel.value == marker.feature.properties.BETRIEB) {
+                console.log(marker.getLatLng())
+                map.setView(marker.getLatLng(), 17)
+                console.log(marker.getPopup())
+                marker.openPopup()
+                console.log(marker.feature.properties.BETRIEB)
+            }
         })
     }
 }
