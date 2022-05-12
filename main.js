97a3f9d69840f875fb9b61f8c118a2cb654aef5d
Nelly Hoppe <nelly.hoppe@student.uibk.ac.at>
2022-05-12 15:59:03 +0200

    Lininefarben implementiert

--- main.js
+++ main.js
@@ -121,7 +121,24 @@ async function loadLines(url) {
     let overlay = L.featureGroup().addTo(map);
     layerControl.addOverlay(overlay, "Liniennetz Vienna Sightseeing");
 
-    L.geoJson(geojson).bindPopup(function (layer) {
+    L.geoJson(geojson, {
+        style: function(feature) {
+            console.log(feature)
+
+            let colors = {
+                "Red Line": "#FF4136 ",
+                "Yellow Line": "#FFDC00",
+                "Blue Line": "#0074D9",
+                "Green Line": "#2ECC40",
+                "Grey Line": "#AAAAAA",
+                "Orange Line": "#FF851B"
+            };
+
+            return {
+                color: `${colors[feature.properties.LINE_NAME]}`
+            }
+        }
+    }).bindPopup(function (layer) {
         return `
             <h4>${layer.feature.properties.LINE_NAME}</h4>
             von: ${layer.feature.properties.FROM_NAME}
